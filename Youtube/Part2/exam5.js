8ea8c62fa782f19d5c61c70b3ce2614b169ad426
MinYoung Lee <avadaisuki68@gmail.com>
2018-07-07 20:17:22 +0900

    example

---
+++ Youtube/Part2/exam5.js
@@ -0,0 +1,39 @@
+var app5 = angular.module('app5', ['weatherFilters']);
+
+app5.controller('mainCtrl', function($scope) {
+	
+	// object array
+	$scope.students = [
+    		{name: "George Thomas", gpa: 3.5},
+	    	{name: "Susy Smith", gpa: 3.6},
+    		{name: "Paul Marks", gpa: 3.2},
+    		{name: "Sue Edgar", gpa: 3.8}
+  	];
+ 	
+	// object array object
+  	$scope.student = {
+      		gpas:[
+        		{name: "George Thomas", gpa: 3.5},
+        		{name: "Susy Smith", gpa: 3.6},
+        		{name: "Paul Marks", gpa: 3.2},
+        		{name: "Sue Edgar", gpa: 3.8}
+      		]
+  	};
+
+	$scope.currDate = new Date();
+
+	$scope.randomStr = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+ 
+  	$scope.randArray = [
+    		"Tomato",
+    		"Potato",
+    		"Bread",
+    		"Pickles",
+    		"Raisins"
+  	];
+ 
+  	$scope.weather = [
+    		{day: "Monday", rain: false},
+    		{day: "Tuesday", rain: true}
+  	];
+});
